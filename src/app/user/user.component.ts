cd82991cd7d2837663359c2a470602a19a199bad
Jacob Deere <kennethdeere2@gmail.com>
2024-07-12 13:45:40 -0500

    changes

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -20,7 +20,6 @@ export class UserComponent {
   UserSelected() {
     //this logs when i select a user but when I make log the varialbe I set it too in app component it wont log it.
 
-    //console.log(this.selected_user);
     this.clickedOn.emit(this.selected_user);
   }
 }
